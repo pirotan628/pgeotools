8f7dd2eb89a413acf24941a73c7c5c292dcc7c6e
pirotan628 <h.otsuka.p3141@gmail.com>
2020-11-06 20:46:10 +0900

    fixed cmdtools

--- cmdtools/pg_plan.py
+++ cmdtools/pg_plan.py
@@ -1,6 +1,6 @@
 from pygc import *
 #from geotools import *
-#import pgeotools as pg
+import pgeotools as pg
 
 nmile = 1852
 
@@ -9,7 +9,7 @@ lon0, lat0 = 134.9953139534642, 34.31518290300008  # South Center
 azm, dist, itr = 90, -0.1 * nmile, 30
 
 #print("{0} {1}".format(lon0,lat0))
-xyarray = make_regular_interval(lon0, lat0, azm, dist, itr)
+xyarray = pg.make_regular_interval(lon0, lat0, azm, dist, itr)
 for i in range(len(xyarray)):
     lon, lat = xyarray[i][0], xyarray[i][1]
     print("{0} {1}".format(lon, lat))
