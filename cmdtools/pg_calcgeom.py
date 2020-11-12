1202606ebdae482ddddf1c83d591985da62f7c7b
pirotan628 <h.otsuka.p3141@gmail.com>
2020-11-12 09:14:02 +0900

    add pg_calcgeom

--- cmdtools/pg_calcgeom.py
+++ cmdtools/pg_calcgeom.py
@@ -1,2 +1,15 @@
+import sys
 import pgeotools as pg
 
+utm_zone = pg.param_config.PROJECT_UTM
+
+lines = sys.stdin
+
+gps_pos = []
+for l in lines:
+    lon,lat = pg.readxy(l)
+    pos = [lon, lat]
+    gps_pos.append(pos)
+
+ship_conf = pg.ship_configuration(17,-0.8,27,2.7)
+coords = pg.calc_geom_from_ship_conf(ship_conf, gps_pos, utm_zone)
